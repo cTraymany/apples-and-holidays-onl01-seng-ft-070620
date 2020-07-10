361068831cd6f61216eff41d57527a8536ff32f4
Chindalath Traymany <chindalatht@gmail.com>
2020-07-10 08:17:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,14 +73,14 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  
   holiday_hash.each do |season, season_hash|
-    season_hash.select do |holiday, supplies|
-      if supplies.include?("BBQ")
-        puts 
+    season_hash.each do |holiday, supplies|
+      holiday.select do |supplies|
+        supplies.include?("BBQ")
       end
     end
   end
+  
 end
 
 
