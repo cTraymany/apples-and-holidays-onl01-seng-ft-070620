433e11e51efa08ad4e32ea4b36322503f4968698
Chindalath Traymany <chindalatht@gmail.com>
2020-07-10 08:25:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,13 +66,10 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  #{ }: #{supplies.join(", ")}"
     end
   end
-
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  
   bbq_holidays = []
   holiday_hash.each do |season, season_hash|
     season_hash.each do |holiday, supplies|
